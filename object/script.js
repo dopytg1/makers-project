3ea63f9cea7c198302c3c3ee7aa45e0be2c37347
tim <tenirberdi444@gmail.com>
2019-06-18 19:28:32 +0600

    add the function in register

--- object/script.js
+++ object/script.js
@@ -1,8 +1,18 @@
 'use strict';
 
+let money , time ;
 
-let money = +prompt ("Ваш бюджет на месяц?", ""),
-    time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+function start (){
+
+         money = +prompt ("Ваш бюджет на месяц?", "");
+         time = prompt ("Введите дату в формате YYYY-MM-DD", "");
+
+         while (money == null || money == "" || isNaN(money)){
+            money = +prompt ("Ваш бюджет на месяц?", "");
+         }
+};
+start();
+
     
 let appData = {
     budget: money,
@@ -10,7 +20,7 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
 
@@ -45,20 +55,50 @@ while( i < 2 ){
         }
 };
 
-appData.moneyPerDay = appData.budget ;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
-alert("Ежедневный бюджет" + appData.moneyPerDay)
-
-if ( appData.moneyPerDay < 100 ){
-    console.log ( "Минимальный достаток" );
-}else if( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
-    console.log ( "Средний достаток" );
-}else if ( appData.moneyPerDay > 2000 ){
-    console.log( "Высокий достаток" );
-}else (){
-    console.log( "Что-то не так ");
+function detectDayBudjet (){
+            alert ("Ваш бюджет на день: " + appData.moneyPerDay);
 };
 
+detectDayBudjet();
+
+
+function detectLevel (){
+    
+    if ( appData.moneyPerDay < 100 ){
+        console.log ( "Минимальный достаток" );
+    }else if( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+        console.log ( "Средний достаток" );
+    }else if ( appData.moneyPerDay > 2000 ){
+        console.log( "Высокий достаток" );
+    }else {
+        console.log( "Что-то не так ");
+    };
+    
+};
+
+
+
+function checkSavings (){
+    if(appData.savings != false){
+        let g = +prompt("Какова сумма накоплений?",""),
+            h = +prompt("Какова процентная ставка","");
+
+        appData.savings = g/100/12*h;
+        alert("Ваш дневной заработок с дивидендов"+ appData.savings);
+    }
+
+};
+
+checkSavings();
+
+function fOptExpences (){
+            for(i=0; i < 3 ; i++ ){
+                let y = prompt("Статья необязательных расходов?","");
+                appData.optionalExpenses ="1: " + y;
+            }
+};
 
 
 
