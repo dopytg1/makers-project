3a650f29db89298ecc4bd84fa263b087e0768d01
tim <tenirberdi444@gmail.com>
2019-07-31 13:13:44 +0600

    new commit

--- react-start/src/App.js
+++ react-start/src/App.js
@@ -16,42 +16,27 @@ class App extends Component {
     }]
   }
 
-  changeName = () => {
-    this.setState({
-      person: [
-        {name:"Mike", age: 29},
-        {name:"Alex", age: 30}
-      ]
-    })
-  }
-  increaseAge = () => {
-    let person = this.state.person.map(item => {
-      return {
-        name: item.name,
-        age: item.age + 1
-      }
-    });
+  deletePerson = (index) => {
+    let person = this.state.person;
+    person.splice(index, 1);
+
     this.setState({person});
   }
 
   render() {
     return (
       <div className="App">
+        {this.state.person.map((item,index) => {
+          return(
         <User 
-        name={this.state.person[0].name} 
-        age={this.state.person[0].age}
-        changeFunc={this.changeName}
-        increaseAge={this.increaseAge}
-        
+            key={index}
+            name={item.name}
+            age={item.age}
+            deletePerson={() => this.deletePerson(index)}
         />
+          )
+        })}
         
-        <User 
-        name={this.state.person[1].name} 
-        age={this.state.person[1].age}
-        changeFunc={this.changeName}
-        
-        
-        />
       </div>
     );
   }
--- react-start/src/Component/User.js
+++ react-start/src/Component/User.js
@@ -1,19 +1,11 @@
 import React from "react";
 
 function User(props) {
-    const incAge = function() {
-            if(props.increaseAge){
-                return(
-                    <button onClick={props.increaseAge}> +1 to age</button>
-                )
-            }
-    }
     return(
         <div className="person">
             <h1>Name: {props.name}</h1>
             <p>Age: {props.age}</p>
-            <button onClick={props.changeFunc}>Change name</button>
-            {incAge()}
+            <button onClick={props.deletePerson}>Delete Person</button>
         </div>
     );
 }
